17f35f953192e41dafec6ebd2573992d4bf48bc5
razat <razatkhadka729@gmail.com>
2024-10-16 11:01:50 +0200

    also added number of counts

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -4,6 +4,7 @@ numbers = [0,1,2,3,4,5,6,7,8,9]
 result = random.choice(numbers)
 guess = int(input("Enter the number : "))
 
+count = 0
 while guess != result:
     if guess +1 > result:
         print("Try again, You guessed Higher Number")
@@ -14,9 +15,7 @@ while guess != result:
     elif guess -1 == result:
           print("Try again, Too Close But High")
     guess = int(input("Enter the number : "))
+    count += 1
 else:
     print("Congratulations, You guesssed the right number")
-
-
- 
-    
+    print(f"total number of attempts : {count}" )
